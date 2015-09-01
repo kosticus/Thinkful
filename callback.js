bbd441b6943da5d20d596f81ee04d13c1103b60c
kosticus <kek2009@gmail.com>
2015-08-31 19:46:13 -0500

    first commit

---
+++ callback.js
@@ -0,0 +1,11 @@
+var numbers = [1, 2, 3];
+
+var doubles = numbers.map(function(num) {
+	return num * 2;
+});
+console.log(doubles);
+
+var add1 = numbers.map(function(num) {
+	return num + 1;
+});
+console.log(add1);
